d5631472dd4eaf2bee0cf628527f366c706b21c4
OniriCorpe <oniricorpe@disroot.org>
2024-06-20 22:35:12 +0200

    auto_rebuild is now an option

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,11 @@
 # PERSONAL HELPERS
 #=================================================
 
+# custom function to change bash bool 0/1 to false/true
+convert_bool(){
+	(("$1")) && echo "true" || echo "false"
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
